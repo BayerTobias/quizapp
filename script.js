1103883f965bcf622f6894c69dd02c3db1168a66
BayerTobias <tbayer2@gmx.de>
2023-05-08 22:07:46 +0200

    js logic additions

--- script.js
+++ script.js
@@ -37,8 +37,13 @@ let currentQuestion = 0;
 let question = questions[currentQuestion];
 
 function init() {
-  renderQuestion();
-  renderFooter();
+  if (currentQuestion >= questions.length) {
+    document.getElementById("quiz-body").style = "display: none;";
+    document.getElementById("end-screen").style = "";
+  } else {
+    renderQuestion();
+    renderFooter();
+  }
 }
 
 function renderQuestion() {
@@ -51,4 +56,37 @@ function renderQuestion() {
 
 function renderFooter() {
   document.getElementById("max_question").innerHTML = questions.length;
+  document.getElementById("current_question").innerHTML = currentQuestion + 1;
+}
+
+function answer(answer) {
+  let rightAnswer = question["right_answer"];
+  let selectedAnswer = answer.slice(-1);
+  let idOfRightAnswer = `answer_${rightAnswer}`;
+  if (selectedAnswer == rightAnswer) {
+    document.getElementById(answer).classList.add("bg-success");
+  } else {
+    document.getElementById(answer).classList.add("bg-danger");
+    document.getElementById(idOfRightAnswer).classList.add("bg-success");
+  }
+  document.getElementById("next_button").disabled = false;
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  question = questions[currentQuestion];
+  resetAnswerButtons();
+  document.getElementById("next_button").disabled = true;
+  init();
+}
+
+function resetAnswerButtons() {
+  document.getElementById("answer_1").classList.remove("bg-success");
+  document.getElementById("answer_1").classList.remove("bg-danger");
+  document.getElementById("answer_2").classList.remove("bg-success");
+  document.getElementById("answer_2").classList.remove("bg-danger");
+  document.getElementById("answer_3").classList.remove("bg-success");
+  document.getElementById("answer_3").classList.remove("bg-danger");
+  document.getElementById("answer_4").classList.remove("bg-success");
+  document.getElementById("answer_4").classList.remove("bg-danger");
 }
